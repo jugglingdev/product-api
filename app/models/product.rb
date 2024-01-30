63cd41266fa68133e46b93afae2bb4a8bd79bd1a
jugglingdev <kaylamarie1785@gmail.com>
2024-01-29 20:25:17 -0600

    WIP

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+    
     validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
     validates :price, numericality: { greater_than: 0 }
 end
